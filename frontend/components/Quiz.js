8485bfe5aa03dda9ba98013987316c87e4e18065
octanym <camjmoore@outlook.com>
2023-09-07 13:37:45 -0400

    implemented quiz fetching behavior

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectAnswer, fetchQuiz } from '../state/action-creators';
 
-export default function Quiz(props) {
+export default function Quiz() {
+  const quiz = useSelector((state) => state.quiz);
+  const dispatch = useDispatch();
+
+  console.log(quiz);
+
+  useEffect(() => {
+    dispatch(fetchQuiz());
+  }, []);
+
   return (
     <div id="wrapper">
       {
         // quiz already in state? Let's use that, otherwise render "Loading next quiz..."
-        true ? (
+        quiz ? (
           <>
             <h2>What is a closure?</h2>
 
             <div id="quizAnswers">
               <div className="answer selected">
                 A function
-                <button>
-                  SELECTED
-                </button>
+                <button>SELECTED</button>
               </div>
 
               <div className="answer">
                 An elephant
-                <button>
-                  Select
-                </button>
+                <button>Select</button>
               </div>
             </div>
 
             <button id="submitAnswerBtn">Submit answer</button>
           </>
-        ) : 'Loading next quiz...'
+        ) : (
+          'Loading next quiz...'
+        )
       }
     </div>
-  )
+  );
 }
