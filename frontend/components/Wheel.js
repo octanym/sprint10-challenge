050b68280582f142e6ef9f7045c056431073b3e2
octanym <camjmoore@outlook.com>
2023-09-07 12:28:42 -0400

    refactored wheel behaviorusing imported actions and redux hooks

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,29 +1,66 @@
-import React, { useState } from "react";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
 
 //get state from store for dynamic value of div cog
 //get action creators to dispatch actions from buttons
 //toggle active class with ternary conditional
 
-export default function Wheel(props) {
-
-  const [wheel, setWheel] = useState(0)
+export default function Wheel() {
+  const wheel = useSelector((state) => state.wheel);
+  const dispatch = useDispatch();
 
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className={ wheel === 0 ? "cog active" : "cog"} style={{ "--i": 0 }}>
-        { wheel === 0 ? "B" : " "}
+        <div
+          className={wheel === 0 ? 'cog active' : 'cog'}
+          style={{ '--i': 0 }}
+        >
+          {wheel === 0 ? 'B' : ' '}
         </div>
-        <div className={ wheel === 1 ? "cog active" : "cog"} style={{ "--i": 1 }}>{ wheel === 1 ? "B" : " "}</div>
-        <div className={ wheel === 2 ? "cog active" : "cog"} style={{ "--i": 2 }}>{ wheel === 2 ? "B" : " "}</div>
-        <div className={ wheel === 3 ? "cog active" : "cog"} style={{ "--i": 3 }}>{ wheel === 3 ? "B" : " "}</div>
-        <div className={ wheel === 4 ? "cog active" : "cog"} style={{ "--i": 4 }}>{ wheel === 4 ? "B" : " "}</div>
-        <div className={ wheel === 5 ? "cog active" : "cog"} style={{ "--i": 5 }}>{ wheel === 5 ? "B" : " "}</div>
+        <div
+          className={wheel === 1 ? 'cog active' : 'cog'}
+          style={{ '--i': 1 }}
+        >
+          {wheel === 1 ? 'B' : ' '}
+        </div>
+        <div
+          className={wheel === 2 ? 'cog active' : 'cog'}
+          style={{ '--i': 2 }}
+        >
+          {wheel === 2 ? 'B' : ' '}
+        </div>
+        <div
+          className={wheel === 3 ? 'cog active' : 'cog'}
+          style={{ '--i': 3 }}
+        >
+          {wheel === 3 ? 'B' : ' '}
+        </div>
+        <div
+          className={wheel === 4 ? 'cog active' : 'cog'}
+          style={{ '--i': 4 }}
+        >
+          {wheel === 4 ? 'B' : ' '}
+        </div>
+        <div
+          className={wheel === 5 ? 'cog active' : 'cog'}
+          style={{ '--i': 5 }}
+        >
+          {wheel === 5 ? 'B' : ' '}
+        </div>
         {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button onClick={() => setWheel((wheel - 1 + 6) % 6)} id="counterClockwiseBtn">Counter clockwise</button>
-        <button onClick={() => setWheel((wheel + 1) % 6)} id="clockwiseBtn">Clockwise</button>
+        <button
+          onClick={() => dispatch(moveCounterClockwise())}
+          id="counterClockwiseBtn"
+        >
+          Counter clockwise
+        </button>
+        <button onClick={() => dispatch(moveClockwise())} id="clockwiseBtn">
+          Clockwise
+        </button>
       </div>
     </div>
   );
