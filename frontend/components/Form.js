db6e1f7f18a3ea20618c2ecb3e5026fa8cefd6d8
octanym <camjmoore@outlook.com>
2023-09-13 22:41:46 -0400

    form component dispatches necessary actions, accurately reflects state and validates form submission criteria

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,26 +1,55 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import * as actionCreators from '../state/action-creators'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { postQuiz, inputChange } from '../state/action-creators';
 
-export function Form(props) {
+export default function Form() {
+  const dispatch = useDispatch();
 
-  const onChange = evt => {
+  const form = useSelector((state) => state.form);
 
-  }
+  const onChange = (evt) => {
+    dispatch(inputChange({ ...form, [evt.target.id]: evt.target.value }));
+  };
 
-  const onSubmit = evt => {
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    dispatch(postQuiz(form));
+  };
 
-  }
+  const valid =
+    form.newQuestion.trim().length > 0 &&
+    form.newTrueAnswer.trim().length > 0 &&
+    form.newFalseAnswer.trim().length > 0;
+
+  console.log(valid);
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn">Submit new quiz</button>
+      <input
+        maxLength={50}
+        onChange={onChange}
+        value={form.newQuestion}
+        id="newQuestion"
+        placeholder="Enter question"
+      />
+      <input
+        maxLength={50}
+        onChange={onChange}
+        value={form.newTrueAnswer}
+        id="newTrueAnswer"
+        placeholder="Enter true answer"
+      />
+      <input
+        maxLength={50}
+        onChange={onChange}
+        value={form.newFalseAnswer}
+        id="newFalseAnswer"
+        placeholder="Enter false answer"
+      />
+      <button id="submitNewQuizBtn" disabled={!valid}>
+        Submit new quiz
+      </button>
     </form>
-  )
+  );
 }
-
-export default connect(st => st, actionCreators)(Form)
