298851943badf2be8e5d64620e435cf5cba88efd
octanym <camjmoore@outlook.com>
2023-09-13 22:48:52 -0400

    minor fix for wheel component values to pass tests

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -13,37 +13,37 @@ export default function Wheel() {
           className={wheel === 0 ? 'cog active' : 'cog'}
           style={{ '--i': 0 }}
         >
-          {wheel === 0 ? 'B' : ' '}
+          {wheel === 0 ? 'B' : null}
         </div>
         <div
           className={wheel === 1 ? 'cog active' : 'cog'}
           style={{ '--i': 1 }}
         >
-          {wheel === 1 ? 'B' : ' '}
+          {wheel === 1 ? 'B' : null}
         </div>
         <div
           className={wheel === 2 ? 'cog active' : 'cog'}
           style={{ '--i': 2 }}
         >
-          {wheel === 2 ? 'B' : ' '}
+          {wheel === 2 ? 'B' : null}
         </div>
         <div
           className={wheel === 3 ? 'cog active' : 'cog'}
           style={{ '--i': 3 }}
         >
-          {wheel === 3 ? 'B' : ' '}
+          {wheel === 3 ? 'B' : null}
         </div>
         <div
           className={wheel === 4 ? 'cog active' : 'cog'}
           style={{ '--i': 4 }}
         >
-          {wheel === 4 ? 'B' : ' '}
+          {wheel === 4 ? 'B' : null}
         </div>
         <div
           className={wheel === 5 ? 'cog active' : 'cog'}
           style={{ '--i': 5 }}
         >
-          {wheel === 5 ? 'B' : ' '}
+          {wheel === 5 ? 'B' : null}
         </div>
         {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
